9e526ffe6ba17048998b78443e510f9f38b35ea2
SKGakiria <swgakiria@gmail.com>
2023-05-31 08:14:43 -0700

    Updated Task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -15,6 +15,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
 	else
 	{
-		return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+		return (binary_tree_size(tree->left) +
+				binary_tree_size(tree->right) + 1);
 	}
 }
